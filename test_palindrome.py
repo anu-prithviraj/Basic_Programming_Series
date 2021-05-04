0641f7f9842c09414cc696e8f4b65d92ee291315
Anu-P-Raj <anu.p@sequoiaat.com>
2021-05-04 15:44:23 +0530

    Fix pytest for RuntimeError case

--- test_palindrome.py
+++ test_palindrome.py
@@ -1,20 +1,32 @@
 from Palindrome import PalindromeFinder
+import pytest
 
 
 def test_to_find_palindrome():
-    dict = {"positive_example" : "Madam, I'm Adam!!", "negative_example" : "Boys"}  
-    expected_for_test_palindrome.pyhappy_case = True 
+
+    dict = {"positive_example" : "Madam, I'm Adam!!", "negative_example" : "Boys","runtime_error":1111}  
+    expected_for_happy_case = True 
     expected_for_unhappy_case = False
     for each_in_dict in dict:
-        pobj = PalindromeFinder(dict[each_in_dict])
         if each_in_dict == "positive_example":
+            pobj = PalindromeFinder(dict[each_in_dict])
             result = pobj.to_find_palindrome()
             assert result == expected_for_happy_case
-        elif each_in_dict == "negative_example":
+            print("Case 1: PASSED")
+        if each_in_dict == "negative_example":
+            pobj = PalindromeFinder(dict[each_in_dict])
             result = pobj.to_find_palindrome()
             assert result == expected_for_unhappy_case    
-        else:
-            raise Exception("Execution completed without test")
+            print("Case 2: PASSED")
+        if each_in_dict == "runtime_error":
+            with pytest.raises(RuntimeError):
+                pobj = PalindromeFinder(dict[each_in_dict])
+            print("Case 2: PASSED")
+    
+
+
+
+      
 
             
     
